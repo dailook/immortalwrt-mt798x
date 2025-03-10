c7d9c0a6fe4737e256beb379560802c606bbec18
dailook <1296762908@qq.com>
2025-03-10 13:51:39 +0800

    Update platform.sh

--- target/linux/mediatek/mt7986/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/mt7986/base-files/lib/upgrade/platform.sh
@@ -150,6 +150,7 @@ platform_do_upgrade() {
 	bananapi,bpi-r3mini |\
 	netcore,n60 |\
         netcore,n60pro |\
+	ikuai,q6000 |\
 	ruijie,rg-x60-pro* |\
 	tplink,tl-xdr608* |\
 	zyxel,ex5700 |\
@@ -183,6 +184,7 @@ platform_check_image() {
 	bananapi,bpi-r3mini* |\
 	netcore,n60 |\
         netcore,n60pro |\
+	ikuai,q6000 |\
 	ruijie,rg-x60-pro* |\
 	*snand* |\
 	glinet,gl-mt6000 |\
