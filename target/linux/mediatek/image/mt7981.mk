b9973beb88edf840b439844d27c6d3826aaaec59
dailook <1296762908@qq.com>
2025-04-20 19:06:51 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -936,3 +936,22 @@ define Device/cmcc_rax3000me
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += cmcc_rax3000me
+
+define Device/sx_7981r128
+  DEVICE_VENDOR := SX
+  DEVICE_MODEL := 7981R128 for zhao
+  DEVICE_DTS := mt7981-sx-7981r128
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
+  SUPPORTED_DEVICES := sx,7981r128
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 116736k
+  UBOOTENV_IN_UBI := 1
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += sx_7981r128
