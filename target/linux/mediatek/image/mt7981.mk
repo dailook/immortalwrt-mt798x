e35e2f747442be62161589e2af464d640cc15db8
dailook <1296762908@qq.com>
2025-05-25 08:15:35 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -962,7 +962,7 @@ define Device/qczt_qc3018ax
   DEVICE_DTS := mt7981-qczt-qc3018ax
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   SUPPORTED_DEVICES := qczt,qc3018ax
-  DEVICE_PACKAGES := $(MT7981_USB_PKGS) f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc mmc-utils \
 	luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
